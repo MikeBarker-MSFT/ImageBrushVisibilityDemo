80f2882cf64415c3921a9b22db3c448efc1e34ce
Mike Barker <mibarker@microsoft.com>
2023-05-07 14:28:43 +0100

    Initial commit

---
+++ ImageBrushVisibilityDemo/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml;
+
+namespace ImageBrushVisibilityDemo
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
